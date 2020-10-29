07b227d350943cd71319dc41b6157321cb34e588
spagnon7 <spagnon7@gmail.com>
2020-10-28 19:35:17 -0700

    finished

---
+++ app.js
@@ -0,0 +1,12 @@
+let helloBtn = document.querySelector('#hello');
+
+helloBtn.addEventListener('click', function() {
+    console.log('hello');
+})
+
+
+let byeBtn = document.querySelector('#goodbye');
+
+byeBtn.addEventListener('click', function() {
+    console.log('goodbye');
+})
